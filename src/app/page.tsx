4736381b6fad9c780dae5dfe3c6d935422fe35fb
Herald Capital Partners <cloud@heraldcappartners.com>
2026-04-21 02:29:02 +0000

    Fix: Services -> Strategies in homepage nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,7 +164,7 @@ export default function HomePage() {
                   whiteSpace: 'nowrap'
                 }}
               >
-                Services
+                Strategies
               </Link>
               <Link 
                 href="/team" 
